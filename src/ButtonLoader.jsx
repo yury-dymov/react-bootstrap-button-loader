d05892c314a60e0a51a55d183e44badce5bdd2c5
Robert Kieffer <r@tend.ai>
2019-06-10 16:35:02 -0700

    Fix #9
    Haven't tested, but I believe this will fix the issue.

--- src/ButtonLoader.jsx
+++ src/ButtonLoader.jsx
@@ -1,6 +1,6 @@
 import React     from 'react';
 import PropTypes from 'prop-types';
-import Button    from 'react-bootstrap/lib/Button';
+import Button    from 'react-bootstrap/Button';
 import Spinner   from './Spinner';
 
 const propTypes = {
